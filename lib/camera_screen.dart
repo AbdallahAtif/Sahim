5b61ec8a9fcbbbfdd57f67478da2b4dfedfdde8a
Abdallah Atef <83876807+AbdallahAtif@users.noreply.github.com>
2023-02-09 19:20:27 +0200

    done
    done

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class CameraScreen extends StatelessWidget {
@@ -11,7 +10,75 @@ class CameraScreen extends StatelessWidget {
     return  Scaffold(
       backgroundColor: HexColor('#060E11'),
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.blueGrey,
+        title: Text('التقط صورة'),
+        actions: [
+          Icon(Icons.close)
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+       
+    
+        children: [
+          Center(child: Image.asset('assets/images/Rectangle 92.png')),
+          SizedBox(height: 30.h,),
+          Row(
+            children: [
+               Padding(
+            padding:  EdgeInsets.only(left: 15.w),
+            child: Container(
+               decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.r)
+              ),
+              width: 162.w,
+              height: 50.h,
+             
+              child: MaterialButton(
+                child: Text(
+                  'اعادة',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 18.sp
+                    ),
+                  ),
+              onPressed: (){}
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 25.w,
+          ),
+          Padding(
+            padding:  EdgeInsets.only(left: 15.w),
+            child: Container(
+              width: 162.w,
+              height: 50.h,
+              
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.r)
+              ),
+              child: MaterialButton(
+                child: Text(
+                  'تمام',
+                  style: TextStyle(
+                    color: HexColor('#2BB9D8'),
+                    fontSize: 18.sp
+                    ),
+                  ),
+              onPressed: (){}
+              ),
+            ),
+          )
+
+            ],
+          ),
+         
+
+      ]
       ),
 
     );
